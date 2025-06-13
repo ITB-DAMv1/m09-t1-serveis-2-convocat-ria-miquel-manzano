e239b874a274d8cce5998fb767a841e49d4977ee
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-06-13 12:56:13 +0200

    DTO Update

--- CompanyEventAPI/CompanyEventClient/DTO/ClientInsertDTO.cs
+++ CompanyEventAPI/CompanyEventClient/DTO/ClientInsertDTO.cs
@@ -5,7 +5,6 @@ namespace CompanyEventClient.DTO
     public class ClientInsertDTO
     {
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public string Empresa { get; set; }
         public string Name { get; set; }
         public string Surename { get; set; }
